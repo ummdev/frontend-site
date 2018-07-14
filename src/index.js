cfc9a3d0cd4594b3def107a364d6c4d65005dfd9
Ryan-Willpower <surasak.c.work@gmail.com>
2018-07-14 18:29:36 +0700

    Add route

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import MainPage from './MainPage'
-
-const Test = ({match}) => (
-  <h3>{match.params.search}</h3>
-)
+import Result from './Result'
 
 const App = () => (
   <Router>
     <div>
-      <Route exactly path="/" component={MainPage} />
-      <Route path="/:search" component={Test}/>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/:search" component={Result} />
     </div>
   </Router>
 )
